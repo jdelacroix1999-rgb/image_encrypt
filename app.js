4d6550aa38b9991327d4ab77a02bc706b79acecd
jdelacroix1999-rgb <jdelacroix1999@gmail.com>
2025-12-27 22:24:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ function enableDownloadOrShare(blob, filename = "permuted.png") {
   }
 }
 
-function scaledDims(w, h, maxDim = 960) {
+function scaledDims(w, h, maxDim = 360) {
   const maxSide = Math.max(w, h);
   if (maxSide <= maxDim) return { w, h, scaled: false };
 
@@ -201,7 +201,7 @@ processBtn.addEventListener("click", async () => {
   try {
     const bmp = await fileToImageBitmap(loadedFile);
 
-    const { w, h } = scaledDims(bmp.width, bmp.height, 960);
+    const { w, h } = scaledDims(bmp.width, bmp.height, 360);
     
     outCanvas.width = w;
     outCanvas.height = h;
